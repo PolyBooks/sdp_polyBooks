a35cf511b5c0396bd9644484480375f8ad5b8a00
Nicolas Raulin <nicolas.raulin@epfl.ch>
2021-03-09 23:39:02 +0100

    Created activity test db

--- app/src/main/java/com/github/polybooks/MainActivity.kt
+++ app/src/main/java/com/github/polybooks/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.github.polybooks
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button_open_db_tests)
+        button.setOnClickListener {
+            startActivity(Intent(this, FirebaseActivity::class.java))
+        }
     }
 }
