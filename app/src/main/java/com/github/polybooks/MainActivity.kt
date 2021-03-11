9c9be44be90ec64427b01e9a6d5094a2f865fd21
Laniakea1999 <zied.mustapha@epfl.ch>
2021-03-11 17:52:20 +0100

    Create Home, login and register activity and links between them

--- app/src/main/java/com/github/polybooks/MainActivity.kt
+++ app/src/main/java/com/github/polybooks/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.github.polybooks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun login_fun(view: View) {
+        setContentView(R.layout.login)
+    }
+
+    fun signup_fun(view: View) {
+        setContentView(R.layout.signup)
+    }
+
+    fun backhome_fun(view: View) {
         setContentView(R.layout.activity_main)
     }
 }
