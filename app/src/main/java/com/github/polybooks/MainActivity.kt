ac0b6517d4af855684e358df4f8ca8f827e76d0a
Joshua Bernimoulin <joshua.bernimoulin@epfl.ch>
2021-03-11 13:38:10 +0100

    Create prototype of database UI. Need test

--- app/src/main/java/com/github/polybooks/MainActivity.kt
+++ app/src/main/java/com/github/polybooks/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.github.polybooks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.dummy_button)
+        button.setOnClickListener { startActivity( Intent(this, DummyDatabaseActivity :: class.java )) }
     }
 }
