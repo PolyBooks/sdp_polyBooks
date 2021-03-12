5e6d9fed8225fbe1c535c53a87c4d5458b636b96
Dario Goedtkindt <dario.goedtkindt@protonmail.ch>
2021-03-12 10:08:40 +0100

    added Interests to the database API

---
+++ app/src/main/java/com/github/polybooks/core/User.kt
@@ -0,0 +1,6 @@
+package com.github.polybooks.core
+
+/**
+ * This class contains all the information to describe a user.
+ * */
+data class User(val uid : Int, val pseudo : String)
--- app/src/main/java/com/github/polybooks/core/database/DatabaseAPI.kt
+++ app/src/main/java/com/github/polybooks/core/database/DatabaseAPI.kt
@@ -5,7 +5,15 @@ import java.util.concurrent.CompletableFuture
 
 /**
  * Provides the API for a database. Allows to retrieve information in the database
- * about Books, Sales.
+ * about Books, Sales, and Interests.
+ *
+ * Books and Sales can be queried with dedicated methods, but more generally,
+ * one can use the queryBooks()/querySales() methods to build a more fine tuned query and execute them.
+ *
+ * The list of what interests exist can be accessed with the dedicated methods. As for the interests
+ * of the user, there is a distinction between the local and remote users. The interest of the user
+ * should be stored in the local Interests, but an authenticated user can save their interests on the
+ * cloud. The database provides methods for saving/restoring local interests to/from the cloud.
  * */
 interface Database {
 
@@ -18,6 +26,58 @@ interface Database {
      * Create a new query for Sales. It originally matches all sales.
      * */
     fun querySales() : SaleQuery
+
+    /**
+     * List all the Fields in the database.
+     * */
+    fun listAllFields() : CompletableFuture<List<Field>>
+
+    /**
+     * List all the Semesters in the database.
+     * */
+    fun listAllSemesters() : CompletableFuture<List<Semester>>
+
+    /**
+     * List all the Courses in the database.
+     * */
+    fun listAllCourses() : CompletableFuture<List<Field>>
+
+    /**
+     * List all the interests in the database.
+     * */
+    fun listAllInterests() : CompletableFuture<Triple<List<Field>, List<Semester>, List<Course>>>
+        = TODO("It can be implemented from the previous functions")
+
+    /**
+     * Get the interests of the local user.
+     * */
+    fun getLocalInterests() : CompletableFuture<Triple<List<Field>, List<Semester>, List<Course>>>
+
+    /**
+     * Sets the interests of the local user.
+     * @return A Future to receive confirmation of success/failure asynchronously
+     * */
+    fun setLocalInterests(interests : List<Interest>) : CompletableFuture<Nothing>
+
+    /**
+     * Get the interests of the given User as stored on the cloud.
+     * TODO: Might need to add an authentication token to restrict authenticated users to only access their interests.
+     * */
+    fun getInterestsFromCloud(user : User) : CompletableFuture<Triple<List<Field>, List<Semester>, List<Course>>>
+
+    /**
+     * Save the interests of the local user to the cloud to the given user's account.
+     * @return A Future to receive confirmation of success/failure asynchronously
+     * TODO: Might need to add an authentication token to restrict authenticated users to only modify their interests.
+     * */
+    fun updateCloudInterestsFromLocal(user : User) : CompletableFuture<Nothing>
+
+    /**
+     * Restore the interests of the local user from the cloud given the user's account:
+     * @return A Future to receive confirmation of success/failure asynchronously
+     * TODO: Might need to add an authentication token to restrict authenticated users to only access their interests.
+     * */
+    fun updateLocalInterestsFromCloud(user : User) : CompletableFuture<Nothing>
 
     /**
      * Get all the books in the database
@@ -52,8 +112,7 @@ interface Database {
      * */
     fun getNSales(numberOfSales : Int, page : Int, ordering : SaleOrdering) : CompletableFuture<List<Sale>>
             = querySales().withOrdering(ordering).getN(numberOfSales, page)
-
-    //TODO need to add methods to modify the database, create entries.
+    
 
 }
 
@@ -78,6 +137,7 @@ interface Query<T> {
     fun getCount() : CompletableFuture<Int>
 
 }
+
 
 /**
  * A BookQuery is a builder for a query to the database that will yield Books.
