82bf03dc5d90d2299ccef10dfa2d59cfbb487c37
Huntsekkern <44240415+Huntsekkern@users.noreply.github.com>
2021-03-15 15:02:12 +0100

    Add get rawValues from an InputImage with an EAN-13 Barcode
    Need to parse ISBN from rawValues and need to add camera access

--- app/src/main/java/com/github/polybooks/ScanBarcode.kt
+++ app/src/main/java/com/github/polybooks/ScanBarcode.kt
@@ -8,7 +8,10 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
 
 
 /*
@@ -16,6 +19,49 @@ This activity open the camera (ask permission for it if not already given) and t
 when it does it scans it, retrieve the ISBN and automatically moves to the FillSale activity passing the ISBN as intent.
  */
 class ScanBarcode : AppCompatActivity() {
+
+    private fun scanBarcodes(image: InputImage) {
+        // [START set_detector_options]
+        // ISBNs are represented on EAN-13 barcodes only.
+        val options = BarcodeScannerOptions.Builder()
+                .setBarcodeFormats(Barcode.FORMAT_EAN_13)
+                .build()
+        // [END set_detector_options]
+
+        // [START get_detector]
+        // Specifying the formats to recognize:
+        val scanner = BarcodeScanning.getClient(options)
+        // [END get_detector]
+
+        // [START run_detector]
+        val result = scanner.process(image)
+                .addOnSuccessListener { barcodes ->
+                    // Task completed successfully
+                    // [START_EXCLUDE]
+                    // [START get_barcodes]
+                    for (barcode in barcodes) {
+                        val bounds = barcode.boundingBox
+                        val corners = barcode.cornerPoints
+
+                        val rawValue = barcode.rawValue
+
+                        val valueType = barcode.valueType
+                        // See API reference for complete list of supported types
+                        when (valueType) {
+                            Barcode.TYPE_ISBN -> {
+                                val displayValue = barcode.displayValue
+                            }
+                        }
+                    }
+                    // [END get_barcodes]
+                    // [END_EXCLUDE]
+                }
+                .addOnFailureListener {
+                    // Task failed with an exception
+                    // ...
+                }
+        // [END run_detector]
+    }
 
     private val myCameraPermission = 1111
 
