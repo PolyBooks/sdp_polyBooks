2281b9550794c8b8b6c9aae30b283af03143ce4f
Sophie du Couédic <>
2021-03-16 10:02:33 +0100

    draft of SearchBook and FilteringBooks Activity

---
+++ app/src/main/java/com/github/polybooks/FilteringBooksActivity.kt
@@ -0,0 +1,13 @@
+package com.github.polybooks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FilteringBooksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filtering_books)
+
+
+    }
+}
