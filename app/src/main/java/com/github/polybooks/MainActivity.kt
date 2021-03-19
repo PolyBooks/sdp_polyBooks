ad42b30da80950ef177b3092b54bd860468cc941
Laniakea1999 <zied.mustapha@epfl.ch>
2021-03-19 11:16:17 +0100

    google authentification

--- app/src/main/java/com/github/polybooks/MainActivity.kt
+++ app/src/main/java/com/github/polybooks/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.github.polybooks
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
 
         val button: Button = findViewById(R.id.button_open_db_tests)
         button.setOnClickListener {
             startActivity(Intent(this, FirebaseActivity::class.java))
         }
-    }
 
-    fun login_fun(view: View) {
-        setContentView(R.layout.login)
+        val button2: Button = findViewById(R.id.log_button)
+        button2.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 
     fun signup_fun(view: View) {
