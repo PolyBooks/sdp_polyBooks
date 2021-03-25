852dda21d2219ccc411e32aaca3865275c7082a7
Laniakea1999 <zied.mustapha@epfl.ch>
2021-03-25 22:52:30 +0100

    Fix warnings in xml activities and write some tests

---
+++ app/src/androidTest/java/com/github/polybooks/LoginGoogleTest.kt
@@ -0,0 +1,44 @@
+package com.github.polybooks
+
+import android.R
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+
+class LoginGoogleTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(LoginActivity::class.java)
+
+    @Before
+    fun before() {
+        Intents.init()
+    }
+
+    @After
+    fun after() {
+        Intents.release()
+    }
+
+    @Test
+    fun signInButtonGoogle() {
+        onView(withId(com.github.polybooks.R.id.sign_in_button)).perform(click())
+    }
+
+    @Test
+    fun signOutButtonGoogle() {
+        onView(withId(com.github.polybooks.R.id.tequila_log_button)).perform(click())
+    }
+}
