164db417b874db521cf9a4550f153c53b99afde8
Dario Goedtkindt <dario.goedtkindt@protonmail.ch>
2021-03-25 14:05:23 +0100

    Refactor interest API

--- app/src/main/java/com/github/polybooks/core/User.kt
+++ app/src/main/java/com/github/polybooks/core/User.kt
@@ -2,5 +2,9 @@ package com.github.polybooks.core
 
 /**
  * This class contains all the information to describe a user.
+ * It can either be the local user (a user that is not logged in),
+ * or a logged in user.
  * */
-data class User(val uid : Int, val pseudo : String)
+sealed class User
+data class LoggedUser(val uid : Int, val pseudo : String) : User()
+object LocalUser : User()
--- app/src/main/java/com/github/polybooks/core/database/InterestDatabase.kt
+++ app/src/main/java/com/github/polybooks/core/database/InterestDatabase.kt
@@ -5,11 +5,6 @@ import java.util.concurrent.CompletableFuture
 
 /**
  * Provides the API for accessing/modifying interests in a database.
- *
- * The list of what interests exist can be accessed with the dedicated methods. As for the interests
- * of the user, there is a distinction between the local and remote users. The interest of the user
- * should be stored in the local Interests, but an authenticated user can save their interests on the
- * cloud. The database provides methods for saving/restoring local interests to/from the cloud.
  * */
 interface InterestDatabase {
 
@@ -35,34 +30,16 @@ interface InterestDatabase {
         = TODO("It can be implemented from the previous functions")
 
     /**
-     * Get the interests of the local user.
-     * */
-    fun getLocalInterests() : CompletableFuture<Triple<List<Field>, List<Semester>, List<Course>>>
-
-    /**
-     * Sets the interests of the local user.
-     * @return A Future to receive confirmation of success/failure asynchronously
-     * */
-    fun setLocalInterests(interests : List<Interest>) : CompletableFuture<Nothing>
-
-    /**
-     * Get the interests of the given User as stored on the cloud.
-     * TODO: Might need to add an authentication token to restrict authenticated users to only access their interests.
-     * */
-    fun getInterestsFromCloud(user : User) : CompletableFuture<Triple<List<Field>, List<Semester>, List<Course>>>
-
-    /**
-     * Save the interests of the local user to the cloud to the given user's account.
-     * @return A Future to receive confirmation of success/failure asynchronously
+     * Get the interests of the specified user
      * TODO: Might need to add an authentication token to restrict authenticated users to only modify their interests.
      * */
-    fun updateCloudInterestsFromLocal(user : User) : CompletableFuture<Nothing>
+    fun getUserInterests(user : User) : CompletableFuture<Triple<List<Field>, List<Semester>, List<Course>>>
 
     /**
-     * Restore the interests of the local user from the cloud given the user's account:
+     * Sets the interests of the specified user.
+     * TODO: Might need to add an authentication token to restrict authenticated users to only modify their interests.
      * @return A Future to receive confirmation of success/failure asynchronously
-     * TODO: Might need to add an authentication token to restrict authenticated users to only access their interests.
      * */
-    fun updateLocalInterestsFromCloud(user : User) : CompletableFuture<Nothing>
+    fun setUserInterests(user : User, interests : List<Interest>) : CompletableFuture<Nothing>
 
 }
