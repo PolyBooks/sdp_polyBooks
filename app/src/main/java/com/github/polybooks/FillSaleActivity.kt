6ba70f50359670b71cc37c5a6849019fc12b7f99
Huntsekkern <44240415+Huntsekkern@users.noreply.github.com>
2021-04-14 21:45:00 +0200

    Add button disabled until sale is complete

--- app/src/main/java/com/github/polybooks/FillSaleActivity.kt
+++ app/src/main/java/com/github/polybooks/FillSaleActivity.kt
@@ -2,9 +2,13 @@ package com.github.polybooks
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import com.github.polybooks.core.Book
 import com.github.polybooks.core.BookCondition
 import com.github.polybooks.core.Sale
@@ -14,6 +18,7 @@ import com.github.polybooks.core.database.interfaces.BookDatabase
 import com.google.firebase.Timestamp
 import java.text.DateFormat
 import java.util.concurrent.CompletableFuture
+
 
 /**
  * This activity receives the ISBN, either manually inputted from AddSale or deduced from the scanned barcode,
@@ -27,7 +32,7 @@ class FillSaleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
     private val bookDB = BookDatabase()
 
     private lateinit var dateFromBookToSale: Timestamp
-    private lateinit var bookConditionSelected: BookCondition
+    private var bookConditionSelected: BookCondition? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +40,7 @@ class FillSaleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
 
 
 
-        val DFormat: DateFormat = DateFormat.getDateInstance(DateFormat.LONG)
+        val dateFormat: DateFormat = DateFormat.getDateInstance(DateFormat.LONG)
 
         // Get the Intent that started this activity and extract the string
         val stringISBN = intent.getStringExtra(ISBN)
@@ -43,24 +48,27 @@ class FillSaleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
         findViewById<TextView>(R.id.filled_publisher)         .apply { text = stringISBN }
 
         // Check if ISBN in our database: (could check ISBN validity before)
-
         val book: CompletableFuture<Book> = bookDB.getBook(stringISBN)
 
         book.thenApply { book ->
             {
-                findViewById<TextView>(R.id.filled_authors)         .apply { text = book.authors?.get(0) ?: "" } //TODO update that to either transform the list to string, and just store the string of authors
+                findViewById<TextView>(R.id.filled_authors)         .apply { text = book.authors?.get(
+                    0
+                ) ?: "" } //TODO update that to either transform the list to string, and just store the string of authors
                 findViewById<TextView>(R.id.filled_title)           .apply { text = book.title }
                 findViewById<TextView>(R.id.filled_edition)         .apply { text = book.edition }
                 findViewById<TextView>(R.id.filled_language)        .apply { text = book.language }
                 findViewById<TextView>(R.id.filled_publisher)       .apply { text = book.publisher }
-                findViewById<TextView>(R.id.filled_publish_date)    .apply { text = DFormat.format(book.publishDate?.toDate()) }
+                findViewById<TextView>(R.id.filled_publish_date)    .apply { text = dateFormat.format(
+                    book.publishDate?.toDate()
+                ) }
                 findViewById<TextView>(R.id.filled_format)          .apply { text = book.format }
 
                 dateFromBookToSale = book.publishDate!!
             }
         }
 
-
+        // Drop-down menu for condition
         val spinner: Spinner = findViewById(R.id.filled_condition)
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
@@ -74,6 +82,27 @@ class FillSaleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
             spinner.adapter = adapter
         }
         spinner.onItemSelectedListener = this
+
+
+        // Disable confirm button until filled
+        disableButton(findViewById<Button>(R.id.confirm_sale))
+
+        findViewById<EditText>(R.id.filled_price).addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (bookConditionSelected != null && findViewById<EditText>(R.id.filled_price).text.toString().isNotEmpty()) {
+                    enableButton(findViewById<Button>(R.id.confirm_sale))
+                } else {
+                    disableButton(findViewById<Button>(R.id.confirm_sale))
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
 
         /* Other spinner implementation
         spinner.onItemSelectedListener = object :
@@ -117,22 +146,45 @@ class FillSaleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
 
     // TODO all the picture stuff.
 
+
     fun confirmSale(view: View) {
-        // TODO determine to which activity we land, but probably not MainActivity but rather a confirmation page
         // store Sale in our database
-        val sale: Sale = Sale(
-                findViewById<TextView>(R.id.filled_title).text.toString(),
-                123, // TODO userID???
-                findViewById<EditText>(R.id.filled_price).text.toString().toFloat(),
-                bookConditionSelected,
-                //findViewById<Spinner>(R.id.filled_condition).getSelectedItem().toString(),
-                dateFromBookToSale,
-                SaleState.ACTIVE
+        val sale = Sale(
+            findViewById<TextView>(R.id.filled_title).text.toString(),
+            123, // TODO userID???
+            findViewById<EditText>(R.id.filled_price).text.toString().toFloat(),
+            // Should never be null as the button is not enabled otherwise
+            bookConditionSelected!!,
+            dateFromBookToSale,
+            SaleState.ACTIVE
         )
         salesDB.addSale(sale)
+
+        // TODO determine to which activity we land, but probably not MainActivity but rather a confirmation page
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
+
+    fun disableButton(button: Button) {
+        button.isEnabled = false
+        button.isClickable = false
+        button.setTextColor(ContextCompat.getColor(applicationContext, R.color.white))
+        button.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.grey))
+    }
+
+    fun enableButton(button: Button) {
+        button.isEnabled = true
+        button.isClickable = true
+        button.setTextColor(ContextCompat.getColor(applicationContext, R.color.black))
+        button.setBackgroundColor(
+            ContextCompat.getColor(
+                applicationContext,
+                R.color.blue_green_400
+            )
+        )
+    }
+
+
 
     /**
      *
@@ -150,14 +202,20 @@ class FillSaleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
      * @param id The row id of the item that is selected
      */
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        val stringCondition: String = parent?.getItemAtPosition(position).toString()
-        when (stringCondition) {
+        when (parent?.getItemAtPosition(position).toString()) {
             "New" -> bookConditionSelected = BookCondition.NEW
             "Good" -> bookConditionSelected = BookCondition.GOOD
             "Worn" -> bookConditionSelected = BookCondition.WORN
             else -> {
-                Toast.makeText(applicationContext,"Error in selecting the Book Condtion",Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext,
+                    "Error in selecting the Book Condtion",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
+        }
+        if (findViewById<EditText>(R.id.filled_price).text.toString().isNotEmpty()) {
+            enableButton(findViewById<Button>(R.id.confirm_sale))
         }
     }
 
@@ -169,6 +227,6 @@ class FillSaleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener
      * @param parent The AdapterView that now contains no selected item.
      */
     override fun onNothingSelected(parent: AdapterView<*>?) {
-        TODO("Not yet implemented")
+        disableButton(findViewById<Button>(R.id.confirm_sale))
     }
 }
