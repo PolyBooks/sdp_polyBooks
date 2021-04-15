6a3cedcd01f968aa2e4907e1beffebe285e57715
Huntsekkern <44240415+Huntsekkern@users.noreply.github.com>
2021-04-15 16:56:36 +0200

    init take_book_picture activity

---
+++ app/src/main/java/com/github/polybooks/TakeBookPictureActivity.kt
@@ -0,0 +1,11 @@
+package com.github.polybooks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TakeBookPictureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_take_book_picture)
+    }
+}
