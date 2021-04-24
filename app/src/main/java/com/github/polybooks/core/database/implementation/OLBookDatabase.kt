ceed35c9723a3768e8839d9e7ea61a9b1e24edf2
Dario Goedtkindt <dario.goedtkindt@epfl.ch>
2021-04-24 18:19:55 +0200

    make OpenLibrary work with books that don't have a full title field but a title field instead

--- app/src/main/java/com/github/polybooks/core/database/implementation/OLBookDatabase.kt
+++ app/src/main/java/com/github/polybooks/core/database/implementation/OLBookDatabase.kt
@@ -24,7 +24,8 @@ import java.util.concurrent.CompletableFuture
 import com.github.polybooks.utils.unwrapException
 
 
-private const val TITLE_FIELD_NAME = "full_title"
+private const val TITLE_FIELD_NAME1 = "full_title"
+private const val TITLE_FIELD_NAME2 = "title"
 private const val AUTHORS_FIELD_NAME = "authors"
 private const val FORMAT_FIELD_NAME = "physical_format"
 private const val ISBN13_FIELD_NAME = "isbn_13"
@@ -120,9 +121,11 @@ class OLBookDatabase(private val url2json : (String) -> CompletableFuture<JsonEl
     @RequiresApi(Build.VERSION_CODES.N)
     private fun parseBook(jsonBook: JsonElement): Book {
         val jsonBookObject = asJsonObject(jsonBook);
-        val title = getJsonField(jsonBookObject, TITLE_FIELD_NAME)
+        val title = getJsonField(jsonBookObject, TITLE_FIELD_NAME1)
+                .map { Optional.of(it) }
+                .orElseGet { getJsonField(jsonBookObject, TITLE_FIELD_NAME2) }
                 .map { parseTitle(it) }
-                .orElseThrow(cantParseException(TITLE_FIELD_NAME))!!
+                .orElseThrow(cantParseException("$TITLE_FIELD_NAME1 or $TITLE_FIELD_NAME2"))!!
         val isbn13 = getJsonField(jsonBookObject, ISBN13_FIELD_NAME)
                 .map { parseISBN13(it) }
                 .orElseThrow(cantParseException(ISBN13_FIELD_NAME))!!
