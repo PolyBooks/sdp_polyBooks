c6ec2071a024dc7b20e4bd90a24f3593165a31d9
Joshua Bernimoulin <joshua.bernimoulin@epfl.ch>
2021-04-29 13:12:04 +0200

    Deleted redundent code

--- app/src/main/java/com/github/polybooks/utils/NavBarCreation.kt
+++ app/src/main/java/com/github/polybooks/utils/NavBarCreation.kt
@@ -13,27 +13,6 @@ import javax.annotation.Nullable
 
 fun setupNavbar(navBar: BottomNavigationView, origin: Context, selectedItem: Int = R.id.default_selected, selectedListener: BottomNavigationView.OnNavigationItemSelectedListener? = null){
     navBar.selectedItemId = selectedItem
-    navBar.setOnNavigationItemSelectedListener { item ->
-        when(item.itemId){
-            R.id.home -> {
-                startActivity(origin,Intent(origin, MainActivity::class.java), null)
-                true
-            }
-            R.id.books ->{
-                startActivity(origin, Intent(origin, FilteringBooksActivity::class.java), null)
-                true
-            }
-            R.id.sales ->{
-                startActivity(origin, Intent(origin, FilteringSalesActivity::class.java), null)
-                true
-            }
-            R.id.user_profile ->{
-                // TODO: user sales
-                false
-            }
-            else -> true
-        }
-    }
 
     selectedListener?.run {
         navBar.setOnNavigationItemSelectedListener(selectedListener)
