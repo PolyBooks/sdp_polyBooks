2ce1d52b4c5f37f50a26599239941f230a322030
Joshua Bernimoulin <joshua.bernimoulin@epfl.ch>
2021-04-29 04:00:06 +0200

    Add tests

--- app/src/androidTest/java/com/github/polybooks/AddSaleActivityTest.kt
+++ app/src/androidTest/java/com/github/polybooks/AddSaleActivityTest.kt
@@ -1,13 +1,17 @@
 package com.github.polybooks
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -21,7 +25,7 @@ class AddSaleActivityTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(AddSaleActivity::class.java)
 
-    /*
+
     @Before
     fun before() {
         Intents.init()
@@ -31,21 +35,58 @@ class AddSaleActivityTest {
     fun after() {
         Intents.release()
     }
-*/
+
     @Test
     fun scanButtonRedirects() {
-        Intents.init()
         onView(withId(R.id.scan_button)).perform(click())
         intended(hasComponent(ScanBarcodeActivity::class.java.name))
-        Intents.release()
     }
 
     @Test
     fun passISBNButtonRedirects() {
-        Intents.init()
         onView(withId(R.id.pass_isbn_button)).perform(click())
         // TODO could check that the ISBN is correctly passed too
         intended(hasComponent(FillSaleActivity::class.java.name))
-        Intents.release()
+    }
+
+    @Test
+    fun navBarSales() {
+        onView(withId(R.id.sales)).perform(click())
+        Intents.intended(IntentMatchers.hasComponent(FilteringSalesActivity::class.java.name))
+    }
+
+    @Test
+    fun navBarProfile() {
+        onView(withId(R.id.user_profile)).perform(click())
+        onView(withId(R.id.pass_isbn_button)).check(ViewAssertions.matches(isDisplayed()))
+    }
+
+    @Test
+    fun navBarBooks() {
+        onView(withId(R.id.books)).perform(click())
+        Intents.intended(IntentMatchers.hasComponent(FilteringBooksActivity::class.java.name))
+    }
+
+    @Test
+    fun navBarDefault() {
+        onView(withId(R.id.default_selected)).check(
+            ViewAssertions.matches(
+                withEffectiveVisibility(
+                    Visibility.GONE
+                )
+            )
+        )
+        onView(withId(R.id.default_selected)).check(ViewAssertions.matches(Matchers.not(isEnabled())))
+    }
+
+    @Test
+    fun navBarSelected() {
+        onView(withId(R.id.default_selected)).check(ViewAssertions.matches(isSelected()))
+    }
+
+    @Test
+    fun navBarHome() {
+        onView(withId(R.id.home)).perform(click())
+        Intents.intended(IntentMatchers.hasComponent(MainActivity::class.java.name))
     }
 }
--- app/src/androidTest/java/com/github/polybooks/FilteringSalesTest.kt
+++ app/src/androidTest/java/com/github/polybooks/FilteringSalesTest.kt
@@ -4,13 +4,16 @@ import android.content.Intent
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.matcher.IntentMatchers.*
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers
 import org.hamcrest.Matchers.*
 import org.junit.*
 
@@ -48,7 +51,7 @@ class FilteringSalesTest {
 
     @Test
     fun clickingOnButtonInViewDoesntCrash() {
-        onView(withId(R.id.title_inc_sort)).perform(click())
+        onView(withId(R.id.title_inc_sort)).perform(scrollTo(), click())
     }
 
     @Test
@@ -180,5 +183,46 @@ class FilteringSalesTest {
         onView(withId(R.id.condition_new)).check(matches(checkFun))
         onView(withId(R.id.condition_good)).check(matches(checkFun))
         onView(withId(R.id.condition_worn)).check(matches(checkFun))
+    }
+
+    @Test
+    fun navBarSales() {
+        onView(withId(R.id.sales)).perform(click())
+        onView(withId(R.id.results_button)).check(ViewAssertions.matches(isDisplayed()))
+    }
+
+    @Test
+    fun navBarProfile() {
+        onView(withId(R.id.user_profile)).perform(click())
+        onView(withId(R.id.results_button)).check(ViewAssertions.matches(isDisplayed()))
+    }
+
+    @Test
+    fun navBarBooks() {
+        onView(withId(R.id.books)).perform(click())
+        Intents.intended(IntentMatchers.hasComponent(FilteringBooksActivity::class.java.name))
+    }
+
+    @Test
+    fun navBarDefault() {
+        onView(withId(R.id.default_selected)).check(
+            ViewAssertions.matches(
+                withEffectiveVisibility(
+                    Visibility.GONE
+                )
+            )
+        )
+        onView(withId(R.id.default_selected)).check(ViewAssertions.matches(Matchers.not(isEnabled())))
+    }
+
+    @Test
+    fun navBarSelected() {
+        onView(withId(R.id.sales)).check(ViewAssertions.matches(isSelected()))
+    }
+
+    @Test
+    fun navBarHome() {
+        onView(withId(R.id.home)).perform(click())
+        Intents.intended(IntentMatchers.hasComponent(MainActivity::class.java.name))
     }
 }
