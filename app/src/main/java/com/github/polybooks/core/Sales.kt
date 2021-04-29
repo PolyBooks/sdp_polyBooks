3d97ef5387bb539dc7ceb2163889748c23ec4a04
Nicolas Raulin <nicolas.raulin@epfl.ch>
2021-04-29 19:07:10 +0200

    Sales and User serialization
    - Sales is temporarily serializable until we find how to deal with
      the pesky timestamps
    - User is now serializable

--- app/src/main/java/com/github/polybooks/core/Sales.kt
+++ app/src/main/java/com/github/polybooks/core/Sales.kt
@@ -23,7 +23,7 @@ data class Sale(
         val seller : User,
         val price : Float,
         val condition : BookCondition,
-        val date : Timestamp,
+        val date : Timestamp?, // TODO remove nullable when timestamp serialization situation will be resolved
         val state : SaleState,
         val image : Image?
         ) : Serializable
