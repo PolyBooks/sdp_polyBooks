c7aecb98495f859d0e7efe5b6d6276198dcef019
Joshua Bernimoulin <joshua.bernimoulin@epfl.ch>
2021-05-06 00:37:18 +0200

    GPS centers on user at launch

--- app/src/main/java/com/github/polybooks/GPSActivity.kt
+++ app/src/main/java/com/github/polybooks/GPSActivity.kt
@@ -1,16 +1,139 @@
 package com.github.polybooks
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.github.polybooks.utils.setupNavbar
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+
 
 class GPSActivity: AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        /*
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+            }
+            shouldShowRequestPermissionRationale(...) -> {
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected. In this UI,
+            // include a "cancel" or "no thanks" button that allows the user to
+            // continue using your app without granting the permission.
+            showInContextUI(...)
+        }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                    Manifest.permission.REQUESTED_PERMISSION)
+            }
+        }
+        locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            locationResult ?: return
+            for (location in locationResult.locations){
+                // Update UI with location data
+                // ...
+            }
+        }
+    }
+
+        */
+
+
         setContentView(R.layout.activity_gps)
+
+        val mapFragment = supportFragmentManager.findFragmentById(
+            R.id.map_fragment
+        ) as? SupportMapFragment
+
+        mapFragment?.getMapAsync { googleMap ->
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                startActivity(Intent(this, MainActivity::class.java))
+           }
+            Log.d("GPSActivity", "Have enabled the thing=============++++++++++++++++===========")
+            googleMap.isMyLocationEnabled = true
+
+            googleMap.setOnMapLoadedCallback {
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location : Location? ->
+                        // Got last known location. In some rare situations this can be null.
+                        location?.let { updateMap(googleMap, location) }
+                    }
+                val epfl = LatLng(46.5165921, 6.5576564)
+
+            }
+
+
+            //fusedLocationClient.requestLocationUpdates()
+
+
+
+
+        }
+
+
+
+
+
 
         setupNavbar(findViewById(R.id.bottom_navigation), this)
     }
+
+    private fun updateMap(googleMap: GoogleMap, location: Location){
+        val current =  LatLng(location!!.latitude, location!!.longitude)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 15f))
+
+    }
+    /*
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopLocationUpdates()
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    fun onLocationChanged(location: Location?) {
+        LatLng(location)
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+    }
+
+ */
 }
