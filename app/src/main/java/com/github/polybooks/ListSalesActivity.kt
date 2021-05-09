409eb026d6de45a710850acaccc67bfb4b3f5917
Joshua Bernimoulin <joshua.bernimoulin@epfl.ch>
2021-05-09 19:40:47 +0200

    Implement filter button and text

--- app/src/main/java/com/github/polybooks/ListSalesActivity.kt
+++ app/src/main/java/com/github/polybooks/ListSalesActivity.kt
@@ -1,8 +1,11 @@
 package com.github.polybooks
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.SystemClock
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -24,21 +27,22 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 /**
  * Activity to list all active sales
+ * Pass boolean extra with id R.string.list_is_sale as false for books
  */
 
 //TODO change name
-class ListSalesActivity : AppCompatActivity() {
+class ListSalesActivity: AppCompatActivity() {
 
     companion object {
-        const val EXTRA_SALE_QUERY_SETTINGS :String = "saleQuerySettings"
-        const val EXTRA_BOOKS_QUERY_SETTINGS : String = "bookQuerySettings"
-        private const val TAG = "GoogleActivity"
+        const val EXTRA_SALE_QUERY_SETTINGS: String = "saleQuerySettings"
+        const val EXTRA_BOOKS_QUERY_SETTINGS: String = "bookQuerySettings"
+        private const val TAG = "ListActivity"
     }
 
     private lateinit var mRecycler: RecyclerView
     private lateinit var mAdapter: RecyclerView.Adapter<*>
     private val mLayout: RecyclerView.LayoutManager = LinearLayoutManager(this)
-    private var isSales :Boolean = true
+    private var isSales: Boolean = true
 
     private val firestore = FirebaseFirestore.getInstance()
     private val olBookDB = OLBookDatabase { string -> url2json(string) }
@@ -47,31 +51,34 @@ class ListSalesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // TODO: rename / change this
         setContentView(R.layout.activity_basic_database)
 
-        isSales = intent.extras?.getBoolean(getString(R.string.list_is_sale), true)?:true
-        Log.d(TAG, "Is sale is $isSales ===============================================")
-        val query : Query<*> =
+        isSales = intent.extras?.getBoolean(getString(R.string.list_is_sale), true) ?: true
+
+        findViewById<TextView>(R.id.sale_or_book).text = if (isSales) getString(R.string.sale) else getString(R.string.books)
+        findViewById<Button>(R.id.filter_button).setOnClickListener { onFilterButtonClick() }
+        val query: Query<*> =
             if (isSales) {
                 intent.getSerializableExtra(EXTRA_SALE_QUERY_SETTINGS)
                     ?.let {
-                        salesDB.querySales().fromSettings(intent.getSerializableExtra(EXTRA_SALE_QUERY_SETTINGS) as SaleSettings)
+                        salesDB.querySales()
+                            .fromSettings(intent.getSerializableExtra(EXTRA_SALE_QUERY_SETTINGS) as SaleSettings)
                     }
                     ?: salesDB.querySales().searchByState(setOf(SaleState.ACTIVE))
             } else {
                 intent.getSerializableExtra(EXTRA_BOOKS_QUERY_SETTINGS)
                     ?.let {
-                        bookDB.queryBooks().fromSettings(intent.getSerializableExtra(EXTRA_BOOKS_QUERY_SETTINGS) as BookSettings)
+                        bookDB.queryBooks()
+                            .fromSettings(intent.getSerializableExtra(EXTRA_BOOKS_QUERY_SETTINGS) as BookSettings)
                     }
                     ?: bookDB.queryBooks()
             }
 
         mRecycler = findViewById(R.id.recyclerView)
         mRecycler.setHasFixedSize(true)
+
         // Links the database api to the recyclerView
-        // TODO: check if completable future is used correctly
-        mAdapter =  if (isSales) SalesAdapter(emptyList()) else BooksAdapter(emptyList())
+        mAdapter = if (isSales) SalesAdapter(emptyList()) else BooksAdapter(emptyList())
         mRecycler.layoutManager = mLayout
         mRecycler.adapter = mAdapter
 
@@ -80,11 +87,18 @@ class ListSalesActivity : AppCompatActivity() {
         setupNavbar(findViewById(R.id.bottom_navigation), this)
     }
 
+    private fun onFilterButtonClick() {
+        if (isSales)
+            startActivity(Intent(this, FilteringSalesActivity::class.java))
+        else
+            startActivity(Intent(this, FilteringBooksActivity::class.java))
+    }
 
-    private fun updateAdapter(list : List<*>){
+    private fun updateAdapter(list: List<*>) {
         runOnUiThread {
-            mAdapter = if (isSales) SalesAdapter(list as List<Sale>) else BooksAdapter(list as List<Book>)
-            mRecycler.adapter= mAdapter
+            mAdapter =
+                if (isSales) SalesAdapter(list as List<Sale>) else BooksAdapter(list as List<Book>)
+            mRecycler.adapter = mAdapter
         }
     }
 }
