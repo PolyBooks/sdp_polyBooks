4b83c47322140474f18f238f75e30b8581b3f1aa
Laniakea1999 <zied.mustapha@epfl.ch>
2021-05-13 00:32:46 +0200

    Main test fixed

--- app/src/androidTest/java/com/github/polybooks/MainTest.kt
+++ app/src/androidTest/java/com/github/polybooks/MainTest.kt
@@ -1,17 +1,13 @@
 package com.github.polybooks
 
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.scrollTo
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.schibsted.spain.barista.assertion.BaristaCheckedAssertions.assertChecked
@@ -21,7 +17,6 @@ import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertD
 import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
 
 import com.schibsted.spain.barista.interaction.BaristaSleepInteractions
-import org.hamcrest.Matchers
 import org.hamcrest.Matchers.not
 import org.junit.After
 import org.junit.Before
@@ -92,10 +87,10 @@ class MainTest {
 
         onView(withId(R.id.button_open_db_tests)).perform(click())
         Intents.intended(IntentMatchers.hasComponent(FilteringSalesActivity::class.java.name))
-      /*
-        onView(withId(R.id.button_open_db_tests)).perform(click())
-        Intents.intended(IntentMatchers.hasComponent(FilteringBooksActivity::class.java.name))
-      */
+        /*
+          onView(withId(R.id.button_open_db_tests)).perform(click())
+          Intents.intended(IntentMatchers.hasComponent(FilteringBooksActivity::class.java.name))
+        */
     }
 
     //This one
@@ -107,47 +102,37 @@ class MainTest {
 
     @Test
     fun navBarSales() {
-        Espresso.onView(ViewMatchers.withId(R.id.sales)).perform(ViewActions.click())
+        onView(withId(R.id.sales)).perform(click())
         Intents.intended(IntentMatchers.hasComponent(FilteringSalesActivity::class.java.name))
     }
 
     @Test
     fun navBarProfile() {
-        Espresso.onView(ViewMatchers.withId(R.id.user_profile)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.add_picture)).perform(scrollTo()).check(
-            ViewAssertions.matches(
-                ViewMatchers.isDisplayed()))
+        onView(withId(R.id.user_profile)).perform(click())
+        onView(withId(R.id.button_open_db_tests)).check(matches(isDisplayed()))
     }
 
     @Test
     fun navBarBooks() {
-        Espresso.onView(ViewMatchers.withId(R.id.books)).perform(ViewActions.click())
+        onView(withId(R.id.books)).perform(click())
         Intents.intended(IntentMatchers.hasComponent(FilteringBooksActivity::class.java.name))
     }
 
     @Test
     fun navBarDefault() {
-        Espresso.onView(ViewMatchers.withId(R.id.default_selected)).check(
-            ViewAssertions.matches(
-                ViewMatchers.withEffectiveVisibility(
-                    ViewMatchers.Visibility.GONE
-                )
-            )
-        )
-        Espresso.onView(ViewMatchers.withId(R.id.default_selected))
-            .check(ViewAssertions.matches(Matchers.not(ViewMatchers.isEnabled())))
+        onView(withId(R.id.default_selected)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+        onView(withId(R.id.default_selected)).check(matches(not(isEnabled())))
     }
 
     @Test
     fun navBarSelected() {
-        Espresso.onView(ViewMatchers.withId(R.id.default_selected))
-            .check(ViewAssertions.matches(ViewMatchers.isSelected()))
+        onView(withId(R.id.home)).check(matches(isSelected()))
     }
 
     @Test
     fun navBarHome() {
-        Espresso.onView(ViewMatchers.withId(R.id.home)).perform(ViewActions.click())
-        Intents.intended(IntentMatchers.hasComponent(MainActivity::class.java.name))
+        onView(withId(R.id.home)).perform(click())
+        onView(withId(R.id.button_open_db_tests)).check(matches(isDisplayed()))
     }
 
 }
