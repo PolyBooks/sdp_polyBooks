d9571f5f84da6d249e12937068f034e2a4baf6a3
Dario Goedtkindt <dario.goedtkindt@epfl.ch>
2021-05-18 18:51:15 +0200

    Modify FirebaseProvider so that maybe it works

--- app/src/main/java/com/github/polybooks/database/FirebaseProvider.kt
+++ app/src/main/java/com/github/polybooks/database/FirebaseProvider.kt
@@ -3,16 +3,14 @@ package com.github.polybooks.database
 import com.google.firebase.firestore.FirebaseFirestore
 
 object FirebaseProvider {
-    private var initialised = false
 
     fun getFirestore() : FirebaseFirestore = FirebaseFirestore.getInstance()
 
     fun getMockFirestore() : FirebaseFirestore {
         val instance = FirebaseFirestore.getInstance()
-        if (!initialised) {
+        try { //Sorry, all the clean ways to prevent calling this twice don't work and I don't know why.
             instance.useEmulator("10.0.2.2", 8080)
-            initialised = true
-        }
+        } catch(_: IllegalStateException) {}
         return instance
     }
 
