f8cbae3391bb1cb58a22f1c3ac601819f8e41f0f
Nicolas Raulin <nicolas.raulin@epfl.ch>
2021-05-20 01:23:56 +0200

    Implemented book rating on sale information
    - Displays the current (mean) rating of a book (supplied by users)
    - Allows users to modify the rating (rate the book)

--- app/src/main/java/com/github/polybooks/activities/SaleInformationActivity.kt
+++ app/src/main/java/com/github/polybooks/activities/SaleInformationActivity.kt
@@ -2,10 +2,17 @@ package com.github.polybooks.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RatingBar
 import android.widget.TextView
+import android.widget.Toast
 import com.github.polybooks.R
+import com.github.polybooks.core.Book
 import com.github.polybooks.core.Sale
+import com.github.polybooks.database.*
 import com.github.polybooks.utils.StringsManip
+import com.github.polybooks.utils.url2json
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 /**
  * This activity displays the detailed product information of a particular
@@ -20,31 +27,61 @@ class SaleInformationActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sale_information)
 
-        val viewTitle: TextView = findViewById(R.id.sale_information_title)
-        val viewEdition: TextView = findViewById(R.id.sale_information_edition)
-        val viewAuthors: TextView = findViewById(R.id.sale_information_authors)
-        // val countryFlag: TextView = findViewById(R.id.countryFlag)
-        val viewPublishDate: TextView = findViewById(R.id.sale_information_book_publish_date)
-        val viewPublisher: TextView = findViewById(R.id.sale_information_book_publisher)
-        val viewFormat: TextView = findViewById(R.id.sale_information_book_format)
-        // val bookImage = findViewById(R.id.proof_picture)
-
-        val viewCondition: TextView = findViewById(R.id.sale_information_condition)
-        val viewPrice: TextView = findViewById(R.id.sale_information_price)
-
-
         val sale = (intent.getSerializableExtra(EXTRA_SALE_INFORMATION) as Sale)
 
-        viewTitle.text = sale.book.title
-        viewEdition.text = sale.book.edition
-        viewAuthors.text = StringsManip.listAuthorsToString(sale.book.authors)
+        findViewById<TextView>(R.id.sale_information_title).text = sale.book.title
+        findViewById<TextView>(R.id.sale_information_edition).text = sale.book.edition
+        findViewById<TextView>(R.id.sale_information_authors).text = StringsManip.listAuthorsToString(sale.book.authors)
+        // val countryFlag: TextView = findViewById(R.id.countryFlag)
+        findViewById<TextView>(R.id.sale_information_book_format).text = sale.book.format
+        // val bookImage = findViewById(R.id.proof_picture)
 
-        viewPublishDate.text = sale.book.publishDate.toString()
+        val ratingBar: RatingBar = findViewById(R.id.sale_information_rating)
+        ratingBar.rating = 0f
 
-        viewPublisher.text = sale.book.publisher
-        viewFormat.text = sale.book.format
+        if (sale.book.totalStars != null && sale.book.numberVotes != null && sale.book.numberVotes != 0) {
+            ratingBar.rating = (sale.book.totalStars / sale.book.numberVotes).toFloat()
+        }
+        ratingBar.setOnRatingBarChangeListener { bar, rating, _ ->
+            val firestore = FirebaseFirestore.getInstance()
+            val olBookDB = OLBookDatabase { string -> url2json(string) }
+            val bookDB = FBBookDatabase(firestore, olBookDB)
 
-        viewCondition.text = sale.condition.name
-        viewPrice.text = sale.price.toString()
+            var query: BookQuery = bookDB.queryBooks()
+            query = query.searchByISBN(setOf(sale.book.isbn))
+            query.getAll().thenCompose { books ->
+                if (books == null || books.isEmpty()) {
+                    throw DatabaseException(
+                        "SaleInformationActivity tried to request information from a Book() " +
+                        "which '${sale.book.isbn}' isbn doesn't exist in database anymore"
+                    )
+                } else if (books.size > 1) {
+                    throw DatabaseException(
+                        "SaleInformationActivity tried to request information from a Book() " +
+                        "which isbn '${sale.book.isbn}' is used ${books.size} times in the database"
+                    )
+                } else {
+                    val book: Book = books[0]
+                    val updatedTotalStars: Double = rating + (book.totalStars ?: 0.0)
+                    val updatedNumberVotes: Int = 1 + (book.numberVotes ?: 0)
+
+                    // bar.rating = (updatedTotalStars / updatedNumberVotes).toFloat()
+                    bar.setIsIndicator(true)
+
+                    bookDB.addBook(
+                        Book(
+                            book.isbn, book.authors, book.title, book.edition,
+                            book.language, book.publisher, book.publishDate,
+                            book.format, updatedTotalStars, updatedNumberVotes
+                        )
+                    )
+                }
+            }
+
+            Toast.makeText(this, "You chose : $rating stars", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<TextView>(R.id.sale_information_condition).text = sale.condition.name
+        findViewById<TextView>(R.id.sale_information_price).text = sale.price.toString()
     }
 }
