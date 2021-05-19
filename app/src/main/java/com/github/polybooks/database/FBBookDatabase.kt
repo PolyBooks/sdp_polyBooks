20ce81c675e1fbf5b3e9f67eb587c1e53dc90efa
Huntsekkern <44240415+Huntsekkern@users.noreply.github.com>
2021-05-20 00:40:02 +0200

    Fix a warning

--- app/src/main/java/com/github/polybooks/database/FBBookDatabase.kt
+++ app/src/main/java/com/github/polybooks/database/FBBookDatabase.kt
@@ -9,6 +9,7 @@ import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.CompletableFuture
 
 /**
@@ -34,7 +35,7 @@ object FBBookDatabase : BookDatabase {
 
     override fun queryBooks(): BookQuery = FBBookQuery()
 
-    private class FBBookQuery() : AbstractBookQuery() {
+    private class FBBookQuery: AbstractBookQuery() {
 
         override fun getAll(): CompletableFuture<List<Book>> {
             when {
