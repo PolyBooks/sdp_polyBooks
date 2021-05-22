f64629063e5d759a89152fe48ba1ecf89127b6e1
Joshua Bernimoulin <joshua.bernimoulin@epfl.ch>
2021-05-22 19:15:40 +0200

    Squelletton, creating files

---
+++ app/src/main/java/com/github/polybooks/activities/EditUserInterestsActivity.kt
@@ -0,0 +1,12 @@
+package com.github.polybooks.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.polybooks.R
+
+class EditUserInterestsActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_user_interests)
+    }
+}
