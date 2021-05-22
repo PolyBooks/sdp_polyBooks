f079ebc61766d4dcead0df924ce8f5957beda347
Joshua Bernimoulin <joshua.bernimoulin@epfl.ch>
2021-05-22 18:48:46 +0200

    Add tests for logged user

--- app/src/androidTest/java/com/github/polybooks/activities/MainTest.kt
+++ app/src/androidTest/java/com/github/polybooks/activities/MainTest.kt
@@ -1,7 +1,9 @@
 package com.github.polybooks.activities
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
@@ -9,6 +11,8 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.*
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.github.polybooks.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 import org.hamcrest.Matchers.not
 import org.junit.*
@@ -27,6 +31,7 @@ class MainTest {
 
     @After
     fun after() {
+        Firebase.auth.currentUser?.delete() // Par securité
         Intents.release()
     }
 
@@ -39,9 +44,8 @@ class MainTest {
         onView(withId(R.id.signup_button)).check(matches(isDisplayed()))
         onView(withId(R.id.signup_button)).check(matches(isClickable()))
 
-        //onView(withId(R.id.sell_button)).check(matches(isDisplayed()))
-        //onView(withId(R.id.sell_button)).check(matches(isClickable()))
-
+        onView(withId(R.id.sell_button)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+        
         onView(withId(R.id.view_books_button)).check(matches(isDisplayed()))
         onView(withId(R.id.view_books_button)).check(matches(isClickable()))
 
@@ -53,12 +57,37 @@ class MainTest {
         intended(hasComponent(LoginActivity::class.java.name))
     }
 
-    @Ignore("Need user to be logged in")
     @Test
     fun sellButton() {
-        onView(withId(R.id.sell_button)).perform(click())
-        intended(hasComponent(AddSaleActivity::class.java.name))
+        onView(withId(R.id.signup_button)).perform(click())
+        onView(withId(R.id.username_field)).perform(
+            scrollTo(),
+            ViewActions.typeText("TestTestTest"),
+            ViewActions.closeSoftKeyboard()
+        )
+        onView(withId(R.id.email_field)).perform(
+            scrollTo(),
+            ViewActions.typeText("test@test.test"),
+            ViewActions.closeSoftKeyboard()
+        )
+        onView(withId(R.id.password1_field)).perform(
+            scrollTo(),
+            ViewActions.typeText("123456"),
+            ViewActions.closeSoftKeyboard()
+        )
+        onView(withId(R.id.password2_field)).perform(
+            scrollTo(),
+            ViewActions.typeText("123456"),
+            ViewActions.closeSoftKeyboard()
+        )
+        onView(withId(R.id.button_reg)).perform(scrollTo(), click())
+        Thread.sleep(1500)
+        onView(withId(R.id.home)).perform(click())
 
+        onView(withId(R.id.signup_button)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+        onView(withId(R.id.log_button)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+        onView(withId(R.id.sell_button)).perform(click())
+        Firebase.auth.currentUser.delete()
     }
 
     @Test
