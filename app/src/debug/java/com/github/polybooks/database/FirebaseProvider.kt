2ab167b8a24ace578239bf3190a2c6f99d013cf1
Joshua Bernimoulin <joshua.bernimoulin@epfl.ch>
2021-06-04 16:27:48 +0200

    Fix test with login on emulator

--- app/src/debug/java/com/github/polybooks/database/FirebaseProvider.kt
+++ app/src/debug/java/com/github/polybooks/database/FirebaseProvider.kt
@@ -1,6 +1,7 @@
 package com.github.polybooks.database
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.firestore.FirebaseFirestore
 
 object FirebaseProvider {
@@ -17,6 +18,8 @@ object FirebaseProvider {
         val instance = FirebaseAuth.getInstance()
         try { //Sorry, all the clean ways to prevent calling this twice don't work and I don't know why.
             instance.useEmulator("10.0.2.2", 9099)
+            instance.createUserWithEmailAndPassword("login@bypassword.test","123456")
+                .addOnSuccessListener { result -> result.user?.updateProfile(userProfileChangeRequest { displayName = "TestLogin" }) }
         } catch(_: IllegalStateException) {}
         return instance
     }
