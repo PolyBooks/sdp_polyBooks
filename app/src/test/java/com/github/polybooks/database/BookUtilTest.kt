f44e065ff3b44d2ebfa45334d3c620003e3e3746
Dario Goedtkindt <dario.goedtkindt@epfl.ch>
2021-06-04 14:16:23 +0200

    test book ordering

--- app/src/test/java/com/github/polybooks/database/BookUtilTest.kt
+++ app/src/test/java/com/github/polybooks/database/BookUtilTest.kt
@@ -2,6 +2,7 @@ package com.github.polybooks.database
 
 import com.github.polybooks.core.Book
 import com.github.polybooks.utils.anonymousBook
+import com.github.polybooks.utils.order
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -15,6 +16,15 @@ class UtilTest {
     @Test
     fun dataBaseExceptionTest(){
         assert(DatabaseException("Hello").message == DatabaseException("Hello").message)
+    }
+
+    @Test
+    fun bookOrderingWorks() {
+        val books = TestBookProvider.books.values.toList()
+        val dec = order(books, BookOrdering.TITLE_DEC)
+        val inc = order(books, BookOrdering.TITLE_INC)
+        assertEquals(dec.reversed(), inc)
+        assertEquals(books.sortedBy { it.title }, inc)
     }
 
 
